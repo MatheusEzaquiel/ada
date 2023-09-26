c5d96ad16903d9db90a4352af8c4fa138652919d
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2023-09-25 21:41:11 -0300

    feat(empresa): add entidade Empresa

---
+++ src/main/java/com/ada/api/domain/empresa/Empresa.java
@@ -0,0 +1,49 @@
+package com.ada.api.domain.empresa;
+
+
+import java.util.List;
+
+import com.ada.api.domain.funcionario.Funcionario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Empresa")
+@Table(name = "empresas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Empresa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String cnpj;
+	private String nome;
+	private String dominio;
+	private String area_atuacao;
+	private String localizacao;
+	private String numero;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	private String pais;
+	private boolean ativo;
+	
+	@OneToMany(mappedBy = "empresa")
+	private List<Funcionario> funcionarios;
+	
+}
