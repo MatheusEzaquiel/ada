f3b70792a6226a954714d271dc61bcae067b6bc9
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2023-10-04 20:42:58 -0300

    feat(cors): adicionando política de CORS

---
+++ src/main/java/com/ada/api/infra/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.ada.api.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/**")
+			//.allowedOrigins("http://localhost:3000")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+		
+	}
+
+}
