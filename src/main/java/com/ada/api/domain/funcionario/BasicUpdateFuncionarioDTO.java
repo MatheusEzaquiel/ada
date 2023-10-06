2cdf93dfdd287ea9d3bc667cc2e6b7bcc1053d46
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2023-10-06 18:48:41 -0300

    refactor(funcionario): add parâmetro nomeCompleto para ser atualizado no DTO

--- src/main/java/com/ada/api/domain/funcionario/BasicUpdateFuncionarioDTO.java
+++ src/main/java/com/ada/api/domain/funcionario/BasicUpdateFuncionarioDTO.java
@@ -1,5 +1,5 @@
 package com.ada.api.domain.funcionario;
 
-public record BasicUpdateFuncionarioDTO(Long id, String apelido, String dataNascimento, String email, String senha, String telefone, String foto) {
+public record BasicUpdateFuncionarioDTO(Long id, String apelido, String nomeCompleto, String dataNascimento, String email, String senha, String telefone, String foto) {
 	
 }
