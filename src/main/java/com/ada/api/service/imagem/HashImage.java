50706ec76af429b2a1151c79c8c7a2ac8eebf95f
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2023-10-12 23:19:15 -0300

    feat(admin): hash das imagens

---
+++ src/main/java/com/ada/api/service/imagem/HashImage.java
@@ -0,0 +1,31 @@
+package com.ada.api.service.imagem;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HashImage {
+
+	public static String hashFileName(String fileName) {
+		
+        try {
+        	
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = md.digest(fileName.getBytes(StandardCharsets.UTF_8));
+
+            // Converte o hash para uma representação hexadecimal
+            StringBuilder hashStringBuilder = new StringBuilder();
+            for (byte b : hashBytes) {
+                hashStringBuilder.append(String.format("%02x", b));
+            }
+
+            return hashStringBuilder.toString();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
