83de1d3c4edaa8dc09ed0396806231ba6a0be7c2
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2023-10-21 23:11:35 -0300

    feat(funcionario): add método reportFuncionarioById() para selecionar dados do funcionário, empresa e cargo do relatório e método getRegistrosDePonto() para selecionar registros de ponto

--- src/main/java/com/ada/api/domain/funcionario/FuncionarioRepository.java
+++ src/main/java/com/ada/api/domain/funcionario/FuncionarioRepository.java
@@ -1,10 +1,14 @@
 package com.ada.api.domain.funcionario;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import com.ada.api.domain.registroDePonto.ReportRegistroDePontoDTO;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 
@@ -33,5 +37,23 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 	        "WHERE f.id = :funcionarioId"
 	        )
 	DetailFuncionarioDTO listFuncionarioJoinEmpresa(@Param("funcionarioId") Long funcionarioId);
-
+	
+	
+	@Query("SELECT new com.ada.api.domain.funcionario.ReportFuncionarioDTO(f.id, f.cpf, f.login, f.nomeCompleto,"
+			+ " f.cargaHorariaDiaria, f.cargaHorariaMensal,"
+			+ " f.horarioEntrada, f.horarioIntervaloEntrada, f.horarioIntervaloSaida, f.horarioSaida,"
+			+ " f.quantidadeFaltas, f.quantidadeFaltasJustificadas, f.quantidadeHorasExtras, f.ativo,"
+			+ " new com.ada.api.domain.empresa.ReportEmpresaDTO(f.empresa.id, f.empresa.cnpj, f.empresa.nome),"
+	        + " new com.ada.api.domain.cargo.ReportCargoDTO(f.cargo.id, f.cargo.area))"
+	        + " FROM Funcionario f"
+	        + " INNER JOIN f.empresa"
+	        + " INNER JOIN f.cargo"
+			+ " WHERE f.id = :funcionarioId"
+			)
+	ReportFuncionarioDTO reportFuncionarioById(@Param("funcionarioId") Long funcionarioId);
+	
+	@Query("SELECT new com.ada.api.domain.registroDePonto.ReportRegistroDePontoDTO("
+			+ "rp.id, rp.data, rp.horarioEntrada, rp.horarioIntervaloEntrada, rp.horarioIntervaloSaida, rp.horarioSaida)"
+	        + " FROM RegistroDePonto rp WHERE rp.funcionario.id = :funcionarioId")
+	List<ReportRegistroDePontoDTO> getRegistrosDePonto(@Param("funcionarioId") Long funcionarioId);
 }
